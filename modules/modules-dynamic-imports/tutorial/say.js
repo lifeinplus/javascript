833c304366a532401ace33499879f6b0ca606494
lifeinplus <lifeinplus@gmail.com>
2020-05-29 20:43:30 +0300

    Учебник JavaScript: Часть 1 - Модули: Динамические импорты

---
+++ modules/modules-dynamic-imports/tutorial/say.js
@@ -0,0 +1,11 @@
+export function hi() {
+    console.log("Hi!");
+}
+
+export function bye() {
+    console.log("Bye!");
+}
+
+export default function () {
+    console.log("Module is loaded (export default)!");
+}
